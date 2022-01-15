5decc8aaabe93cdc2bf1c1117737b868b0a1d9c8
BFSmx4 <97541925+BFSmx4@users.noreply.github.com>
2022-01-14 21:41:29 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,13 +16,13 @@ function setup() {
   
   createCanvas(windowWidth,windowHeight);
 
-  //adding the background image
+  //Añadir la imagen de fondo
   bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
 bg.addImage(bgImg)
 bg.scale = 1.1
   
 
-//creating the player sprite
+//Crear el sprite del jugador
 player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
  player.addImage(shooterImg)
    player.scale = 0.3
@@ -45,7 +45,7 @@ function draw() {
 
 
 
-  //moving the player up and down and making the game mobile compatible using touches
+  //Mover al jugador arriba y abajo y hacer que el juego móvil sea compatible de forma táctil.
 if(keyDown("UP_ARROW")||touches.length>0){
   player.y = player.y-30
 }
@@ -54,14 +54,14 @@ if(keyDown("DOWN_ARROW")||touches.length>0){
 }
 
 
-//release bullets and change the image of shooter to shooting position when space is pressed
+//Liberar las balas y cambiar la imagen del tirador a la posición de disparo cuando la barra espaciadora se presiona.
 if(keyWentDown("space")){
  
   player.addImage(shooter_shooting)
  
 }
 
-//player goes back to original standing image once we stop pressing the space bar
+//El jugador regresa a la imagen de posición original una vez que dejamos de presionar la barra de espacio.
 else if(keyDown("space")){
   //player.addImage( shooter_shooting )
  // player.addImage()
